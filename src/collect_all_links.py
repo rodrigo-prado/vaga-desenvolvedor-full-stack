7365f47de6783fd7be858eef10432d5a3238d62d
Rodrigo-Alves <Rodrigo.Alves@ibm.com>
2020-08-22 20:07:46 -0300

    Refactoring. Removing `get_list_of_all_links`. Implementing the logic in the main method. Now, acessing, and storing information in the database.

--- src/collect_all_links.py
+++ src/collect_all_links.py
@@ -1,38 +1,23 @@
 #!/usr/bin/env python3
 
+import sys
 import argparse
-import psycopg2
+# import psycopg2
 import requests
 from bs4 import BeautifulSoup
 from validator_collection import checkers
-from database import verify_connection, delete_records
+from database import verify_connection, delete_records, insert_link, get_next_not_tracked_link, \
+    mark_link_as_tracked, get_number_of_stored_links
 
-def get_list_of_all_links(web_link, history_links, max_links):
+def get_next_web_url():
     """
-    This function is responsible for get the list of the links, store, and search for more links.
+    This function is responsible for get the next link and mark the link as tracked in the database
     """
-    if (checkers.is_url(web_link) and web_link not in history_links):
-        print(web_link)
+    web_link = get_next_not_tracked_link()
+    if web_link is not None:
+        mark_link_as_tracked(web_link)
 
-        # Add the link to the history
-        history_links.append(web_link)
-
-        if (len(history_links) >= max_links):
-            return
-
-        page = requests.get(web_link)
-        soup = BeautifulSoup(page.content, 'html.parser')
-
-        # Get all links
-        href_links = [href.get('href') for href in soup.find_all('a')]
-
-        # Print all links found
-        # for link in href_links:
-        #     print(link)
-
-        # Get links from referenced links
-        for link in href_links:
-            get_list_of_all_links(link, history_links, max_links)
+    return web_link
 
 
 if __name__ == '__main__':
@@ -42,18 +27,48 @@ if __name__ == '__main__':
                         action='store',
                         type=str,
                         help='initial web link to collect other links')
-    parser.add_argument('--max-links',
+    parser.add_argument('--limit',
                         action='store',
                         type=int,
                         default=100,
-                        help='max number of links (default to 100)')
+                        help='a limit to avoid endless search for links (default to 100)')
     arg = parser.parse_args()
-    print('Initial web link:', arg.web_link)
 
-    if (verify_connection()):
-        print('Database connection is OKay!')
-        delete_records()
-        history_links = []
-        get_list_of_all_links(arg.web_link, history_links, arg.max_links)
-    else:
+    # Check if the initial web link is valid; if not, terminate
+    if not checkers.is_url(arg.web_link):
+        print('Initial web link is not valid!')
+        sys.exit(1)
+
+    # Check if the database connection is OKay; if not, terminate
+    if not verify_connection():
         print('Database connection problem!')
+        sys.exit(1)
+
+    # Initializing link table
+    print('Deleting old records.')
+    delete_records()
+    print('Inserting initial web link:', arg.web_link)
+    insert_link(arg.web_link)
+
+    web_link = get_next_web_url()
+    print(web_link)
+    # history_links = []
+    while web_link is not None:
+        page = requests.get(web_link)
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        # Get all links
+        href_links = [href.get('href') for href in soup.find_all('a')]
+
+        for link in href_links:
+            if checkers.is_url(link):
+                link_id = insert_link(link)
+                if link_id is not None:
+                    print('Web link:', link, 'inserted')
+
+        # Get next link to collect new links
+        web_link = get_next_web_url()
+
+        # Check if the number of stored link is bigger then limit; if yes break the loop
+        if get_number_of_stored_links() > arg.limit:
+            break
--- src/database.py
+++ src/database.py
@@ -2,20 +2,138 @@
 import psycopg2
 from config import config
 
+def get_number_of_stored_links():
+    """ Return the quantity of the link that was stored in the database """
+    conn = None
+    number_of_links = None
+    try:
+        # Read database configuration
+        params = config()
+        # Connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # Create a new cursor
+        cur = conn.cursor()
+        # Select the next non-tracked link
+        cur.execute("SELECT COUNT(*) FROM link")
+        row = cur.fetchone()
+        if row is not None:
+            number_of_links = row[0]
+        # Close communication with the PostgreSQL database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return number_of_links
+
+def mark_link_as_tracked(link_url):
+    """ Mark link as tracked """
+    sql = """ UPDATE link
+                SET tracked = 1
+                WHERE url = %s"""
+    conn = None
+    updated_rows = 0
+    try:
+        # Read database configuration
+        params = config()
+        # Connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # Create a new cursor
+        cur = conn.cursor()
+        # Execute the UPDATE  statement
+        cur.execute(sql, (link_url,))
+        # Get the number of updated rows
+        updated_rows = cur.rowcount
+        # Commit the changes to the database
+        conn.commit()
+        # Close communication with the PostgreSQL database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return updated_rows
+
+
+def get_next_not_tracked_link():
+    """ Return the next link that was not tracked yet """
+    conn = None
+    link_url = None
+    try:
+        # Read database configuration
+        params = config()
+        # Connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # Create a new cursor
+        cur = conn.cursor()
+        # Select the next non-tracked link
+        cur.execute("SELECT url FROM link WHERE tracked = 0 ORDER BY id")
+        row = cur.fetchone()
+        if row is not None:
+            link_url = row[0]
+        # Close communication with the PostgreSQL database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return link_url
+
+
+def insert_link(link_url):
+    """ insert a new link into the link table """
+    sql = """INSERT INTO link(url, tracked)
+             VALUES(%s, 0) RETURNING id;"""
+    conn = None
+    link_id = None
+    try:
+        # Read database configuration
+        params = config()
+        # Connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # Create a new cursor
+        cur = conn.cursor()
+        # Verify existent link URL
+        cur.execute("SELECT url FROM link WHERE url = %s ORDER BY id", (link_url,))
+        row = cur.fetchone()
+        if row is None:
+            # Insert a new link URL
+            cur.execute(sql, (link_url,))
+            # get the generated id back
+            link_id = cur.fetchone()[0]
+            # Commit the changes to the database
+            conn.commit()
+        # Close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return link_id
+
+
 def delete_records():
     """ Delete all records of links in the PostgreSQL database"""
     conn = None
     try:
-        # read the connection parameters
+        # Read the connection parameters
         params = config()
-        # connect to the PostgreSQL server
+        # Connect to the PostgreSQL server
         conn = psycopg2.connect(**params)
         cur = conn.cursor()
-        # delete link table data
+        # Delete link table data
         cur.execute("DELETE FROM link")
-        # close communication with the PostgreSQL database server
+        # Close communication with the PostgreSQL database server
         cur.close()
-        # commit the changes
+        # Commit the changes
         conn.commit()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
@@ -29,25 +147,20 @@ def verify_connection():
     conn = None
     return_value = True
     try:
-        # read connection parameters
+        # Read connection parameters
         params = config()
-
-        # connect to the PostgreSQL server
+        # Connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
         conn = psycopg2.connect(**params)
-
-        # create a cursor
+        # Create a cursor
         cur = conn.cursor()
-
-        # execute a statement
+        # Execute a statement
         print('PostgreSQL database version:')
         cur.execute('SELECT version()')
-
-        # display the PostgreSQL database server version
+        # Display the PostgreSQL database server version
         db_version = cur.fetchone()
         print(db_version)
-
-        # close the communication with the PostgreSQL
+        # Close the communication with the PostgreSQL
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
