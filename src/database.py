a7a492d3eede44a7e98ace38dafe6c3c1ae27376
Rodrigo-Alves <Rodrigo.Alves@ibm.com>
2020-08-22 12:15:12 -0300

    Adding `delete_records` to erase all data before start searching for links

--- src/database.py
+++ src/database.py
@@ -2,6 +2,28 @@
 import psycopg2
 from config import config
 
+def delete_records():
+    """ Delete all records of links in the PostgreSQL database"""
+    conn = None
+    try:
+        # read the connection parameters
+        params = config()
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        # delete link table data
+        cur.execute("DELETE FROM link")
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
 def verify_connection():
     """ Connect to the PostgreSQL database server """
     conn = None
